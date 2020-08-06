c806316b7a2a64b58d13d08f37d7b61030f1ed5e
Aatmjeet Singh <39636595+Aatmjeet@users.noreply.github.com>
2020-08-06 17:42:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -16,31 +16,6 @@ function createWindow(){
       nodeIntegration: true
     }
   })
-  /*win.on('close', (event) => {
-    event.preventDefault()
-    let options = {
-          type: 'question',
-          buttons: ['Yes', 'No'],
-          title: 'Confirm',
-          message: 'Are you sure you want to quit?'
-          //cancelId: 1
-        }
-    var choice = dialog.showMessageBox(win, options);
-    if(choice == 0){
-      win.destroy();
-      //app.quit;
-      //win = null;
-    }
-    //dialog.showMessageBox(win, options, (res) => {
-      //console.log(res);
-      //console.log(checked);
-      //if (res == 1){
-        //event.preventDefault()
-        //Yes button pressed
-        //win.destroy();
-        //win = null;
-        //app.quit();
-  })*/
   win.loadFile('index.html')
   win.setMenu(null)
   // Open the DevTools.
@@ -48,14 +23,6 @@ function createWindow(){
     win.show()
   })
   //win.webContents.openDevTools()
-  //win.on('closed', () => {
-  	//console.log("closed");
-    //win = null;
-    //app.quit();
- 	//})
- //Listen close event to show dialog message box
-	
-  	//})
 }
   
 app.whenReady().then(createWindow)
@@ -69,7 +36,6 @@ ipcMain.on('start', (event, arg) => {
     accountName: name,
     password: pass,
     twoFactorCode: auth
-    //twoFactorCode: SteamTotp.generateAuthCode(config.bot2.sharedSecret)
     };
   client.logOn(logOnOptions);
   client.on('loggedOn', () => {
@@ -91,8 +57,6 @@ app.on('window-all-closed', () => {
 
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow()
   }
